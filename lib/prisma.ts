e25bfe5a73c174b4c55102cfcf29353ee43b5c5a
Jay <jayant.motwani@c2fo.com>
2026-06-06 22:17:13 +0530

    fix: prisma vercel build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,13 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient()
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
