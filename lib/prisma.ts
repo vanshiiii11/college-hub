8ad82601769cdc2fc8cfd4ec937d3854716e0d08
Jay <jayant.motwani@c2fo.com>
2026-06-06 22:24:03 +0530

    fix: prisma client generation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+export const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
